d230bae094ad8f9422643a8eb8fe6c5fabff602e
Babu Thomas <babuthomas138@gmail.com>
2018-02-25 21:53:34 +0530

    Send email via sendgrid

--- msg_project/settings.py
+++ msg_project/settings.py
@@ -129,4 +129,9 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
