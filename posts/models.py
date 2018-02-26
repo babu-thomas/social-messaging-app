205a315d9fabf2bb5e3b1c1dc162c6df0389394b
Babu Thomas <babuthomas138@gmail.com>
2018-02-26 22:23:07 +0530

    Create Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    message = models.CharField(max_length=280)
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.id])
+
+    def __str__(self):
+        return self.message
