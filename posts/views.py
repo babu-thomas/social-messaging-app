b2480c20fd5ae33cc49be34aeedc6db1de410256
Babu Thomas <babuthomas138@gmail.com>
2018-02-28 06:36:05 +0530

    Make template, views, urls for post list

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from . import models
+
+
+class PostListView(ListView):
+    model = models.Post
+    template_name = 'post_list.html'
