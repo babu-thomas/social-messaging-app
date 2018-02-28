56002df86ec5edd589a903f06c54a07c0cea5a00
Babu Thomas <babuthomas138@gmail.com>
2018-02-28 08:21:42 +0530

    Create views and urls for post update, delete, detail

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from . import models
 
@@ -6,3 +8,17 @@ from . import models
 class PostListView(ListView):
     model = models.Post
     template_name = 'post_list.html'
+
+class PostDetailView(DetailView):
+    model = models.Post
+    template_name = 'post_detail.html'
+
+class PostUpdateView(UpdateView):
+    model = models.Post
+    fields = ['message']
+    template_name = 'post_edit.html'
+
+class PostDeleteView(DeleteView):
+    model = models.Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
