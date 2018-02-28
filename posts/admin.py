82f7ac60a5da1e0bc462368ea0b9c9c261dc8074
Babu Thomas <babuthomas138@gmail.com>
2018-02-28 13:29:03 +0530

    Use StackedInline to show comments on posts in admin site

--- posts/admin.py
+++ posts/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Post)
+
+class CommentInline(admin.StackedInline):
+    model = models.Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(models.Post, PostAdmin)
 admin.site.register(models.Comment)
