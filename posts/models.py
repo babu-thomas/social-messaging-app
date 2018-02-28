b85a7e2f0e9f8dc4f5455736e0340ef48fe443b3
Babu Thomas <babuthomas138@gmail.com>
2018-02-28 13:12:56 +0530

    Create comment model

--- posts/models.py
+++ posts/models.py
@@ -16,3 +16,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.message
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.comment
